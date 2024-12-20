dfe27b4959c0272af971ed1fbbac9df7b33e4f69
song <1023001754@qq.com>
2024-12-21 02:23:38 +0800

    test AnimatedContent SizeTransform

--- composeApp/src/commonMain/kotlin/compose/project/demo/common/test/TestCommon015AnimatedContent.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/common/test/TestCommon015AnimatedContent.kt
@@ -1,16 +1,19 @@
 package compose.project.demo.common.test
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import compose.project.demo.common.test.collect.TestCase
 
@@ -74,7 +78,6 @@ object TestCommon015AnimatedContent : TestCase<TestCommon015AnimatedContent> {
             }
 
             AnimatedContent(
-                modifier = Modifier.fillMaxSize(),
                 targetState = state,
                 transitionSpec = {
                     if ((initialState + 1) % list.size == targetState % list.size) {
@@ -113,10 +116,22 @@ object TestCommon015AnimatedContent : TestCase<TestCommon015AnimatedContent> {
                                 fullWidth
                             },
                         )
-                    }
+                    } using SizeTransform(
+                        sizeAnimationSpec = { initialSize, targetSize ->
+                            tween(
+                                durationMillis = 3000,
+                                easing = LinearEasing,
+                            )
+                        }
+                    )
                 },
             ) { targetState ->
-                list[targetState]()
+                Box(
+                    modifier = Modifier.fillMaxHeight(if (targetState % 2 == 0) 0.5f else 1f)
+                        .border(5.dp, if (targetState % 2 == 0) Color.Green else Color.Magenta),
+                ) {
+                    list[targetState]()
+                }
             }
         }
     }
