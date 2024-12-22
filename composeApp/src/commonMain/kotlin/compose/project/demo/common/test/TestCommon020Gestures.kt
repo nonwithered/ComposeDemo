fbb7caf9c3837f5a5dae858aed5d32a48c9ee578
song <1023001754@qq.com>
2024-12-23 02:25:51 +0800

    test transformable

--- composeApp/src/commonMain/kotlin/compose/project/demo/common/test/TestCommon020Gestures.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/common/test/TestCommon020Gestures.kt
@@ -4,10 +4,18 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.selection.selectable
@@ -22,10 +30,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.round
 import androidx.compose.ui.unit.sp
 import compose.project.demo.common.test.collect.TestCase
+import compose.project.demo.common.utils.intOffset
 
 @OptIn(ExperimentalFoundationApi::class)
 object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
@@ -36,6 +48,8 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
         { TestSelectable() },
         { TestCombinedClickable() },
         { TestTriStateToggleable() },
+        { TestDraggable() },
+        { TestTransformable() },
     )
 
     @Composable
@@ -66,7 +80,7 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
                 .background(Color.Blue)
                 .clickable {
                     state++
-                }
+                },
         )
     }
 
@@ -84,7 +98,7 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
                 .background(Color.Blue)
                 .toggleable(state) {
                     state = it
-                }
+                },
         )
     }
 
@@ -107,7 +121,7 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
                     .background(Color.Blue)
                     .selectable(state == 0) {
                         state = 0
-                    }
+                    },
             )
             Text(
                 text = "selectable ${ state == 1 }",
@@ -119,7 +133,7 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
                     .background(Color.Blue)
                     .selectable(state == 1) {
                         state = 1
-                    }
+                    },
             )
         }
     }
@@ -152,7 +166,7 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
                     onClick = {
                         click++
                     },
-                )
+                ),
         )
     }
 
@@ -172,7 +186,72 @@ object TestCommon020Gestures : TestCase<TestCommon020Gestures> {
                         ToggleableState.Off -> ToggleableState.Indeterminate
                         ToggleableState.Indeterminate -> ToggleableState.On
                     }
-                }
+                },
         )
+    }
+
+    @Composable
+    private fun BoxScope.TestDraggable() {
+        var deltaX by remember {
+            mutableStateOf(0f)
+        }
+        var deltaY by remember {
+            mutableStateOf(0f)
+        }
+        Text(
+            text = "draggable",
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.Center)
+                .offset {
+                    deltaX intOffset deltaY
+                }
+                .background(Color.Blue)
+                .draggable(
+                    orientation = Orientation.Vertical,
+                    state = rememberDraggableState { delta ->
+                        deltaY += delta
+                    },
+                ).draggable(
+                    orientation = Orientation.Horizontal,
+                    state = rememberDraggableState { delta ->
+                        deltaX += delta
+                    },
+                ),
+        )
+    }
+
+    @Composable
+    private fun BoxScope.TestTransformable() {
+        var scale by remember { mutableStateOf(1f) }
+        var rotation by remember { mutableStateOf(0f) }
+        var offset by remember { mutableStateOf(IntOffset.Zero) }
+        val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+            scale *= zoomChange
+            offset += offsetChange.round()
+            rotation += rotationChange
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .fillMaxHeight(0.9f)
+                .transformable(state)
+                .background(Color.LightGray),
+        ) {
+            Text(
+                text = "transformable",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                modifier = Modifier.align(Alignment.Center)
+                    .offset { offset }
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                        rotationZ = rotation
+                    }
+                    .background(Color.Blue),
+            )
+        }
     }
 }
