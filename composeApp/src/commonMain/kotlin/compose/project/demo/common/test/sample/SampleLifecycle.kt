6e3fddbca8ad4a34e5f7b4139fb30faf1b31b1fc
song <1023001754@qq.com>
2025-01-05 23:49:19 +0800

    sample local

--- composeApp/src/commonMain/kotlin/compose/project/demo/common/test/sample/SampleLifecycle.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/common/test/sample/SampleLifecycle.kt
@@ -1,50 +1,65 @@
 package compose.project.demo.common.test.sample
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.RememberObserver
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 import compose.project.demo.common.test.collect.TestCase
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import compose.project.demo.common.test.collect.TestCase.Companion.TAG
+import compose.project.demo.common.utils.logE
+import composedemo.composeapp.generated.resources.Res
+import composedemo.composeapp.generated.resources.sample_res_string
+import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.stringResource
 
 object SampleLifecycle : TestCase<SampleLifecycle> {
 
+//    val LocalString = compositionLocalOf<String> { error("stub") }
+    val LocalString = staticCompositionLocalOf<String> { error("stub") }
+
     @Composable
     override fun BoxScope.Content() {
+        TAG.logE { "CCCCC" }
+        var style by remember {
+            mutableStateOf(false)
+        }
+        LaunchedEffect(Unit) {
+            delay(2000L)
+            style = true
+        }
+        CompositionLocalProvider(
+            LocalString provides (if (style) "Q" else "W"),
+        ) {
+            A()
+        }
     }
 
     @Composable
-    private fun SampleLaunchedEffect(a: String, b: Long, c: Double, d: Any) {
-        LaunchedEffect(a, b, c, d) {
-            // This is a suspend block.
-            coroutineScope {
-                // Do something.
-            }
-            launch {
-                // Do something.
-            }
-        }
+    private fun BoxScope.A() {
+        TAG.logE { "AAAAA" }
+        B()
     }
-    private class RememberObserverMap<K, V>: MutableMap<K, V> by hashMapOf(), RememberObserver {
-        override fun onRemembered() { /* TODO */ }
-        override fun onForgotten() { /* TODO */ }
-        override fun onAbandoned() { /* TODO */ }
+
+    @Composable
+    private fun BoxScope.B() {
+        TAG.logE { "BBBBB" }
+        Text(
+            text = LocalString.current,
+            modifier = Modifier.align(Alignment.Center),
+        )
     }
 }
