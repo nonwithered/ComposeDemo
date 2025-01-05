2b7f9a08999ac7690f0f43a8bcccde514da43d92
song <1023001754@qq.com>
2025-01-06 01:49:15 +0800

    sample snapshotFlow

--- composeApp/src/commonMain/kotlin/compose/project/demo/common/test/sample/SampleKoin.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/common/test/sample/SampleKoin.kt
@@ -2,15 +2,22 @@ package compose.project.demo.common.test.sample
 
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import compose.project.demo.common.test.collect.TestCase
 import compose.project.demo.common.test.collect.TestCase.Companion.TAG
 import compose.project.demo.common.utils.logD
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import org.koin.compose.koinInject
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.dsl.scopedOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -61,5 +68,38 @@ object SampleKoin : TestCase<SampleKoin> {
         TAG.logD { "4 ${b.a.id}" }
 //        unloadKoinModules(m)
 
+        SampleSnapshotFlow()
+
+        var count by remember {
+            mutableStateOf(0)
+        }
+
+        val isPositive by remember {
+            derivedStateOf {
+                count > 0
+            }
+        }
+    }
+
+    private val sampleFlow = MutableStateFlow(0)
+
+    @Composable
+    private fun SampleSnapshotFlow() {
+        val countState = remember {
+            mutableStateOf(0)
+        }
+        LaunchedEffect(countState) {
+            snapshotFlow {
+                countState.value
+            }.collect { value ->
+                sampleFlow.value = value
+            }
+        }
+        var count by countState
+        // Ignore some code
+    }
+
+    private fun println(msg: Any?) {
+        TAG.logD { "${msg}" }
     }
 }
